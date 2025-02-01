e39a89e9818dbe6ae04d8f88e77a5c74f80b29b5
supersuperMax <59124796+supersuperMax@users.noreply.github.com>
2025-02-01 17:23:47 +0300

    generic views
    add generic views

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,15 @@
 from django.db.models import F
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
+# from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 # Create your views here.
 
-# def detail(request, question_id):
-#     return HttpResponse("You're looking at question %s." % question_id)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-# def results(request, question_id):
-#     return HttpResponse("You're looking at the results of question %s." % question_id)
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
-# def vote(request, question_id):
-#     return HttpResponse("You're voting on question %s." % question_id)
+class 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -45,9 +31,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))       
-   
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)    
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))  
