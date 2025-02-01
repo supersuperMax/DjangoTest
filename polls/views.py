96cc937acb00093471dc2c4fc29e78dffca4c41e
supersuperMax <59124796+supersuperMax@users.noreply.github.com>
2025-02-01 17:46:55 +0300

    test
    add some test for model Question

--- polls/views.py
+++ polls/views.py
@@ -9,7 +9,20 @@ from .models import Choice, Question
 
 # Create your views here.
 
-class 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
